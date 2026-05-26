3ca235bf080e6df9254c4d64995df101decda6b4
EasinTanvir <easintanvir102@gmail.com>
2026-05-26 15:57:59 +0600

    setup server

---
+++ authService/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+// Morgan middleware
+app.use(morgan("dev"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Server is running on port 3000");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
