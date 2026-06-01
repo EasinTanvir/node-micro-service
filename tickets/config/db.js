5ae6ef9c7c47db60dcae57e45ded9e86410c95db
EasinTanvir <easintanvir102@gmail.com>
2026-06-01 19:33:48 +0600

    load db url from env

--- tickets/config/db.js
+++ tickets/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://ticket-mongo-service:27017/ticket");
+    await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB Connected for tickets");
   } catch (error) {
